9fb0611b703c3954fbaf0c912443e0b921384bdc
arman1104 <arman051215@gmail.com>
2025-11-14 14:23:49 +0530

    made some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
               <img
                 src={product.image}
                 alt={product.title}
-                className="w-full h-48 sm:w-full sm:h-36 lg:w-full lg:h-36 xl:w-56 xl:h-32 object-contain mx-auto my-6 sm:my-5"
+                className="w-56 h-48 sm:w-full sm:h-36 lg:w-full lg:h-36 xl:w-56 xl:h-32 object-contain mx-auto my-6 sm:my-5"
               />
 
               {/* ---------- TEXTS ---------- */}
